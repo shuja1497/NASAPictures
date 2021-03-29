555b7e1960edc6068ef1c126d9735b5cbf576b8d
shuja1497 <shujareshi1497@gmail.com>
2021-03-29 12:25:02 +0530

    added @Dao and room-ktx depndcy

--- NASAPictures/app/src/main/java/com/example/nasapictures/data/db/PictureDao.kt
+++ NASAPictures/app/src/main/java/com/example/nasapictures/data/db/PictureDao.kt
@@ -1,10 +1,12 @@
 package com.example.nasapictures.data.db
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.nasapictures.model.Picture
 
+@Dao
 interface PictureDao {
 
     @Insert
